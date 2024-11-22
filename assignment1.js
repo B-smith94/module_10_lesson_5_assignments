1f963e4dc058e99fffefcdbad7355ad5cf3f7b08
Byron Smith <byrondsmith9@gmail.com>
2024-11-22 14:37:37 -0500

    Tried to implement a loop to display products on the webpage, but failed, so reverted to previous code specific to this array configuration.

--- assignment1.js
+++ assignment1.js
@@ -13,13 +13,15 @@ Task 3: Implement event listener to trigger display of products when the page lo
 let products = [
     { name: "Laptop", price: 999.99, description: "Powerful computing on the go" },
     { name: "Smartphone", price: 699.99, description: "Stay connected wherever you are" },
-    { name: "Headphones", price: 149.99, description: "Immersive audio experience" },
-    { name: "Xbox Series X", price: 449.99, description: "Elevate your gaming" }
+    { name: "Headphones", price: 149.99, description: "Immersive audio experience" }
 ];
 
 // Task 2
 function displayProducts() {
-    products.forEach((product) => console.log(product));
+    document.getElementById('products').innerHTML = 
+    products[0].name + ", " + products[0].price + ", " +products[0].description + "; " + 
+    products[1].name + ", " + products[1].price + ", " +products[1].description + "; " + 
+    products[2].name + ", " + products[2].price + ", " +products[2].description;
     console.log("Displaying Products...")
 }
 
